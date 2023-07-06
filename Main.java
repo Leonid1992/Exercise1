e29cd5754422d3844503952cd9a29fd41006602f
Leonid <leonid.vilcu@vlc2.com>
2023-07-06 15:42:52 +0200

    Esercise 2 finito

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        String[] array = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday" , "Friday", "Saturday" };
+
+        System.out.println(array.length);
+        System.out.println(array[3]);
+    }
+
+}
